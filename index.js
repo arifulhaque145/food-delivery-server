06c9f61532f5ce739c9246568e0582c31461456e
Ariful145 <arifulh145@gmail.com>
2021-10-31 08:16:52 +0600

    database

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbdvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const userCollection = client.db("delivery").collection("users");
+
+  console.log("db connected");
+  console.error(userCollection.dir);
+  client.close();
+});
+
 app.get("/", async (req, res) => {
   res.send("This is main");
 });
