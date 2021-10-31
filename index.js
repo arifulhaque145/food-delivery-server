27ff9153e46564a41e621f1a7f0fff1e2c6aaa0a
Ariful145 <arifulh145@gmail.com>
2021-10-31 17:28:26 +0600

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbdvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 app.use(cors());
@@ -16,19 +17,61 @@ const client = new MongoClient(uri, {
 
 client.connect((err) => {
   const userCollection = client.db("delevery_db").collection("users");
+  const productCollection = client.db("delevery_db").collection("products");
 
+  // INSERT TO DB
   app.post("/users", async (req, res) => {
     const newUser = req.body;
     const result = await userCollection.insertOne(newUser);
     res.send(result);
   });
 
+  // LOAD USERS API
   app.get("/users", async (req, res) => {
     const result = await userCollection.find({}).toArray();
     res.json(result);
   });
+  
+  // LOAD PRODUCTS API
+  app.get("/products", async (req, res) => {
+    const result = await productCollection.find({}).toArray();
+    res.json(result);
+  });
 
-  // console.error(userCollection.dir);
+  // LOAD USER WITH ID API
+  app.get("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await userCollection.findOne(query);
+    // console.log("load user id", id);
+    res.send(result);
+  });
+
+  // UPDATE USER API
+  app.put("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const updatedUser = req.body;
+    const filter = { _id: ObjectId(id) };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: {
+        name: updatedUser.name,
+        email: updatedUser.email,
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc, options);
+    // console.log("Updating user", updatedUser);
+    res.json(result);
+  });
+
+  // DELETE API
+  app.delete("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await userCollection.deleteOne(query);
+    res.json(result);
+  });
+
   // client.close();
 });
 
